08b8c27d4dec98a12e10a9724e5aef312077910c
Priit Laht <priit.laht@nortal.com>
2015-12-22 20:56:00 +0200

    encode header urls

--- src/main/java/com/priitlaht/ppwebtv/frontend/common/util/PaginationUtil.java
+++ src/main/java/com/priitlaht/ppwebtv/frontend/common/util/PaginationUtil.java
@@ -3,8 +3,10 @@ package com.priitlaht.ppwebtv.frontend.common.util;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 
 import lombok.NoArgsConstructor;
 
@@ -22,19 +24,25 @@ public final class PaginationUtil {
   public static HttpHeaders generatePaginationHttpHeaders(Page<?> page, String baseUrl) throws URISyntaxException {
     HttpHeaders headers = new HttpHeaders();
     headers.add("X-Total-Count", "" + page.getTotalElements());
+    String safeBaseUrl = baseUrl;
+    try {
+      safeBaseUrl = URLEncoder.encode(baseUrl, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
     String link = "";
     if ((page.getNumber() + 1) < page.getTotalPages()) {
-      link = "<" + (new URI(baseUrl + "?page=" + (page.getNumber() + 1) + "&size=" + page.getSize())).toString() + ">; rel=\"next\",";
+      link = "<" + (new URI(safeBaseUrl + "?page=" + (page.getNumber() + 1) + "&size=" + page.getSize())).toString() + ">; rel=\"next\",";
     }
     if ((page.getNumber()) > 0) {
-      link += "<" + (new URI(baseUrl + "?page=" + (page.getNumber() - 1) + "&size=" + page.getSize())).toString() + ">; rel=\"prev\",";
+      link += "<" + (new URI(safeBaseUrl + "?page=" + (page.getNumber() - 1) + "&size=" + page.getSize())).toString() + ">; rel=\"prev\",";
     }
     int lastPage = 0;
     if (page.getTotalPages() > 0) {
       lastPage = page.getTotalPages() - 1;
     }
-    link += "<" + (new URI(baseUrl + "?page=" + lastPage + "&size=" + page.getSize())).toString() + ">; rel=\"last\",";
-    link += "<" + (new URI(baseUrl + "?page=" + 0 + "&size=" + page.getSize())).toString() + ">; rel=\"first\"";
+    link += "<" + (new URI(safeBaseUrl + "?page=" + lastPage + "&size=" + page.getSize())).toString() + ">; rel=\"last\",";
+    link += "<" + (new URI(safeBaseUrl + "?page=" + 0 + "&size=" + page.getSize())).toString() + ">; rel=\"first\"";
     headers.add(HttpHeaders.LINK, link);
     return headers;
   }
